b60f9db44d45bc7c44d5da4142845097d66579aa
IsaqueLi <isakilica16@gmail.com>
2023-06-09 14:22:25 -0300

    implementando componete banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './Banner.css'
+
+export const Banner = () => {
+    return(
+        <header className="banner">
+            <img src="./imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    )
+}
+
---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,5 @@
+//esse metodo e utilizado em questao de organizacao de como o codigo e importado.
+import { Banner } from "./Banner"
+
+export default Banner
+
