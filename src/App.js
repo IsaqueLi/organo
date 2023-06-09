b043682ad66c3223702ff45466c3564298b68ed4
IsaqueLi <isakilica16@gmail.com>
2023-06-09 14:29:03 -0300

    implementando arquivos principais app.js index.js e index.css do src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario />
+    </div>
+  )
+}
+
+export default App
