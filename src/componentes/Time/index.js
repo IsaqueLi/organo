2e003a5e2d975e6516858a64dc54c44725d337b4
IsaqueLi <isakilica16@gmail.com>
2023-06-14 11:29:45 -0300

    implementando funcionalidade de exibir somente as listas que contem colaboradores cadastrados

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -7,7 +7,7 @@ const Time = (props) => {
     const css = { backgroundColor: props.corSecundaria }
 
     return (
-        <section className='time' style={css}>
+        props.colaboradores.length > 0 && <section className='time' style={css}>
             <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map( colaborador => <Colaborador key={props.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
